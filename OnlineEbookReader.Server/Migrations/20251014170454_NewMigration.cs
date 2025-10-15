c8f68087c36cc8f5605f86ff13dc34c0edf300a3
git <catalin1second@gmail.com>
2025-10-15 09:16:37 +0300

    database changes

---
+++ OnlineEbookReader.Server/Migrations/20251014170454_NewMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineEbookReader.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Books");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Books",
+                table: "Books",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Books",
+                table: "Books");
+
+            migrationBuilder.RenameTable(
+                name: "Books",
+                newName: "Products");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+        }
+    }
+}
